2b7a29df5c19564f32ec399256355ef6d9db5fa9
Sunah <csa4550@gmail.com>
2022-02-17 21:12:00 +0900

    2022-02-17

---
+++ DealimUniv2/src/test/java/com/dealim/univ/DealimUnivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dealim.univ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealimUnivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
